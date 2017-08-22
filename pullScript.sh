e4fc74e7005a3c3ede4a4f1875000e1ed059627a
Hakon Schutt <hakon@schutt.no>
2017-08-22 13:08:14 +0200

    Automateted push before merge

--- pullScript.sh
+++ pullScript.sh
@@ -6,10 +6,15 @@ git commit -m "Automateted push before merge"
 git push --all
 echo "Pushed everything to your personal branch"
 echo 
-echo "Checkingout master before upstream pull"
+echo
 git checkout master 
 git pull upstream master
+git add --all
+git commit -m "automatet commit after pull"
+git push --all
 git checkout mergebranch
 git merge master
+git add --all
+git commit -m "automatet merge with master after pull"
 echo "everything seems to be working fine"
 
