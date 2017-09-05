cdd234d3a8d8b246c82d8ca1d404c017ecd2717f
Hakon Schutt <hakon@schutt.no>
2017-09-05 08:22:23 +0200

    Automateted push before merge

--- pullScript.sh
+++ pullScript.sh
@@ -1,14 +1,14 @@
-# git add --all
-# git commit -m "Automateted push before merge"
-# git push --all
-# git checkout master 
-# git pull upstream master
-# git add --all
-# git commit -m "automatet commit after pull"
-# git push --all
-# git checkout mergebranch
-# git merge master
-# git add --all
-# git commit -m "automatet merge with master after pull"
-# git push --all
-# echo "everything seems to be working fine"
+git add --all
+git commit -m "Automateted push before merge"
+git push --all
+git checkout master 
+git pull upstream master
+git add --all
+git commit -m "automatet commit after pull"
+git push --all
+git checkout mergebranch
+git merge master
+git add --all
+git commit -m "automatet merge with master after pull"
+git push --all
+echo "everything seems to be working fine"
